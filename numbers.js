bf670d3c3f9695dd47ac44e1203f77ebf2c36d32
Hannah Nguyen <hnguyen160801@gmail.com>
2023-05-08 19:52:14 -0400

    add facts to html

--- numbers.js
+++ numbers.js
@@ -1,6 +1,7 @@
-"use strict"
+"use strict";
 
 const BASE_URL = "http://numbersapi.com";
+const $main = $("main");
 
 /**
  * Gets a fact about a number
@@ -16,12 +17,12 @@ async function getFactAboutNumber(num) {
 /**
  * Gets one fact about each number in nums
  * @param  {...number} nums
- * @returns {string[]} array of facts (strings)
+ * @returns {string{}}} object of facts (strings)
  */
 async function getFactsAboutNumbers(...nums) {
     const response = await axios.get(`${BASE_URL}/${nums.join(",")}?json`);
 
-    return response.data
+    return response.data;
 }
 
 /**
@@ -39,3 +40,39 @@ async function getFactsAboutNumber(num, howMany) {
 
     return (await Promise.all(requests)).map(response => response.data.text);
 }
+
+/**
+ * displays a fact for each number in nums array
+ * @param {number[]} nums
+ */
+async function displayFactsAboutNumbers(nums) {
+    const facts = await getFactsAboutNumbers(nums);
+    const $header = $("<h1>Facts About Many Numbers</h1>");
+
+    $main.append($header);
+
+    for (let fact in facts) {
+        const $div = $(`<div>${fact}: ${facts[fact]}</div>`);
+        $main.append($div);
+    }
+}
+
+/**
+ * displays howMany amount of facts for num
+ * @param {number} num an int
+ * @param {number} howMany an int, how many facts to get
+ */
+async function displayFactsAboutNumber(num, howMany) {
+    const facts = await getFactsAboutNumber(num, howMany);
+    const $header = $("<h1>Many Facts About One Number</h1>");
+
+    $main.append($header);
+
+    for (let fact of facts) {
+        const $div = $(`<div>${fact}</div>`);
+        $main.append($div);
+    }
+}
+
+displayFactsAboutNumbers([7, 8, 9]);
+displayFactsAboutNumber(8, 4);
